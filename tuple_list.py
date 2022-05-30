2f53d3475428bab92b0cd68b6ae513fcc73d9e51
tusharsaini144 <105995276+tusharsaini144@users.noreply.github.com>
2022-05-31 00:17:53 +0530

    Create tuple_list.py

---
+++ tuple_list.py
@@ -0,0 +1,16 @@
+# Python program to add a tuple to list 
+
+# Creating the List
+myList = [9, 3, 1, 4]
+
+# Printing the List 
+print("Initially List : " + str(myList))
+
+# Creating Tuple
+myTuple = (2, 6)
+
+# Adding the tuple to list
+myList += myTuple
+
+# Printing resultant List
+print("List after Addition : " + str(myList))
