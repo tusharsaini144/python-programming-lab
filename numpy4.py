cb4a428806dc289f53559e0fad922f3ca9ea879d
tusharsaini144 <105995276+tusharsaini144@users.noreply.github.com>
2022-05-31 01:58:26 +0530

    Create numpy4.py

---
+++ numpy4.py
@@ -0,0 +1,11 @@
+#Write a NumPy program to multiply the values ​​of two given vectors.
+import numpy as np
+x = np.array([1, 8, 3, 5])
+print("Vector-1")
+print(x)
+y= np.random.randint(0, 11, 4)
+print("Vector-2")
+print(y)
+result = x * y
+print("Multiply the values of two said vectors:")
+print(result)
