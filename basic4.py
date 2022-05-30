afed9fd2f909929745002c33384a048ae0cabadd
tusharsaini144 <105995276+tusharsaini144@users.noreply.github.com>
2022-05-31 02:33:45 +0530

    Create basic4.py

---
+++ basic4.py
@@ -0,0 +1,9 @@
+#Python program for swapping the value of two integers
+x = float(input('ENTER THE VALUE OF X: '))
+y = float(input('ENTER THE VALUE OF Y: '))
+
+temp = x
+x = y
+y = temp
+
+print('X :', x,' Y :', y)
