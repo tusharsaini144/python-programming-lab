c05f035fb027f11754afc448ec7fcfd23f2ac0cf
tusharsaini144 <105995276+tusharsaini144@users.noreply.github.com>
2022-05-31 00:15:40 +0530

    Create tuple_length.py

---
+++ tuple_length.py
@@ -0,0 +1,11 @@
+# Python program to find the size of a tuple 
+
+# Creating a tuple in python 
+myTuple = ('includehelp', 'python', 3, 2021)
+
+# Finding size of tuple using len() method 
+tupleLength = len(myTuple)
+
+# Printing the tuple and Length
+print("Tuple : ", str(myTuple))
+print("Tuple Length : ", tupleLength)
