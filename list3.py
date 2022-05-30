68d28d3ea8314fb963de9cbe178822aab5d94e78
tusharsaini144 <105995276+tusharsaini144@users.noreply.github.com>
2022-05-31 01:14:04 +0530

    Create list3.py

---
+++ list3.py
@@ -0,0 +1,20 @@
+# define list 
+list = [10, 20, 30, 40, 50, 60]
+
+start = 1
+end = 4
+
+if ( start < 0):
+    print "Invalid start index"
+    quit()
+
+if( end > len(list)-1):
+    print "Invalid end index"
+    quit ()
+
+# create another list
+list1 = list[start:end+1]
+
+# printth lists 
+print "list : ", list
+print "list1: ", list1
