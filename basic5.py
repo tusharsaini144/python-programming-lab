fc650e5d6c366c0bf17e2542788cd9f6ff2abe95
tusharsaini144 <105995276+tusharsaini144@users.noreply.github.com>
2022-05-31 02:34:44 +0530

    Create basic5.py

---
+++ basic5.py
@@ -0,0 +1,18 @@
+#Python program to find standard deviation
+def stdv(X):
+    mean = sum(X)/len(X)
+    tot = 0.0
+    for x in X:
+        tot = tot + (x - mean)**2
+    return (tot/len(X))**0.5
+
+# main code
+#  a simple data-set 
+sample = [1, 2, 3, 4, 5] 
+print("Standard Deviation of the sample is: ", stdv(sample))
+
+sample = [1, 2, 3, -4, -5] 
+print("Standard Deviation of the sample is: ", stdv(sample))
+
+sample = [10, -20, 30, -40, 50] 
+print("Standard Deviation of the sample is: ", stdv(sample))
