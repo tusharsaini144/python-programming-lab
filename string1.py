c485da2516ec3e56dfd993f489c0532bd87e7fd3
tusharsaini144 <105995276+tusharsaini144@users.noreply.github.com>
2022-05-31 00:56:29 +0530

    Create string1.py

---
+++ string1.py
@@ -0,0 +1,12 @@
+# Split string using for loop
+
+# function to split string
+def split_str(s):
+  return [ch for ch in s]
+
+# main code  
+string = "Hello world!"
+
+print("string: ", string)
+print("split string...")
+print(split_str(string))
